b7b93215e12fa4daca8fa391eebced49f3ea3355
jennicruz1307 <111473472+jennicruz1307@users.noreply.github.com>
2022-09-06 21:03:21 -0500

    Update population-dispersion.py

--- population-dispersion.py
+++ population-dispersion.py
@@ -1,9 +1,5 @@
-# Jennifer Cruz Population-dispersion
-
 import statistics
 
 statistics.pvariance([1, 3, 4, 2, 6, 5, 3, 4, 5, 2])
 
 statistics.pstdev([1, 3, 4, 2, 6, 5, 3, 4, 5, 2])
-
-#Jennifer Cruz
